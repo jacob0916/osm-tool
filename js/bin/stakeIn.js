e1a859196085de67f956ea63c9d63de152509617
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2020-09-02 16:30:05 +0800

    update

--- js/bin/stakeIn.js
+++ js/bin/stakeIn.js
@@ -13,6 +13,9 @@ smStartIndex
 smCount
 
  */
+let {buildOpenGrpData, buildStakeInData, getTxReceipt, sendTx} = require('./util/wanchain');
+const config = require('../cfg/config');
+const web3 = new Web3(new Web3.providers.HttpProvider(config.wanNodeURL));
 
 const optimist = require('optimist');
 let argv = optimist
@@ -26,9 +29,80 @@ let argv = optimist
     .default('amount', 2000)
     .argv;
 
-function main() {
+async function main() {
     // only admin can open group.
     console.log(argv);
+    let grpId = getGrpIdByString(argv.gid);
+    let smStartIndex = argv.smsi;
+    let smCount = argv.smcnt;
+    let msgValue = web3.utils.toBN(argv.amount);
+
+    // waddr, wkaddr, wkpk, enodeId
+    let ret = [];
+    let linesRelation = await processLineByLine(config.RelationList);
+
+    for (let i = smStartIndex; i < smStartIndex + smCount; i++) {
+        console.log(linesRelation[i]);
+        await doStake(split(linesRelation[i])[0],
+            config.smgScAddr,
+            msgValue,
+            grpId,
+            split(linesRelation[i])[2],
+            split(linesRelation[i])[3],);
+    }
+    console.log("========================done=========================");
+
+}
+
+async function doStake(walletAddr, smgAddr, msgValue, grpId, wkPk, enodeId) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await  buildStakeInData(grpId, wkPk, enodeId);
+            let txHash = await sendTx(walletAddr, smgAddr, msgValue, data);
+            resolve(txHash);
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
+
+function stringTobytes32(name) {
+    let b = Buffer.alloc(32)
+    b.write(name, 32 - name.length, 'utf8')
+    let id = '0x' + b.toString('hex')
+    return id
+}
+
+
+function getGrpIdByString(str) {
+    return stringTobytes32(str);
+}
+
+async function processLineByLine(fileName) {
+    return new Promise((resolve, reject) => {
+
+        try {
+            let lines = [];
+            const rl = readline.createInterface({
+                input: fs.createReadStream(fileName),
+                crlfDelay: Infinity
+            });
+
+            rl.on('line', (line) => {
+                lines.push(line)
+            });
+            rl.on('close', () => {
+                resolve(lines);
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+function split(line, sep = '\t') {
+    return line.split(sep);
 }
 
 main();
